749561b605cb63c677f45088fed0cf74dc6f74c8
mehedi-hasan-patwary <mehedi501245@gmail.com>
2026-07-14 22:57:24 +0600

    Understanting the differnce between js and ts

---
+++ First program/index.js
@@ -0,0 +1,8 @@
+"use strict";
+let userName = "mehedi Hasasn";
+//userName=123; //error ashto
+console.log(userName);
+function addNumbers(num1, num2) {
+    console.log(num1 + num2);
+}
+addNumbers(20, 10);
---
+++ First program/index.ts
@@ -0,0 +1,11 @@
+let userName="mehedi Hasasn";
+
+
+//userName=123; //error ashto
+
+console.log(userName);
+
+function addNumbers(num1:number,num2:number){
+    console.log(num1+num2);
+}
+addNumbers(20,10);
