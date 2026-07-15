f6c49dbc47a94e27985ae65f66546baa601d950b
mehedi-hasan-patwary <mehedi501245@gmail.com>
2026-07-16 01:08:49 +0600

    Data Type

---
+++ Tuple Data Type/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let user;
+user = [100, "auiwd qiwd"];
+console.log(user);
+user.push(1011, "Mehedi hasan");
+console.log(user);
+// console.log(user[2]);  //error
---
+++ Tuple Data Type/index.ts
@@ -0,0 +1,6 @@
+ let user :[number,string];
+ user=[100,"auiwd qiwd"];
+ console.log(user);
+ user.push(1011,"Mehedi hasan");
+ console.log(user);
+// console.log(user[2]);  //error
